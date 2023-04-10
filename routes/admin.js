61b22b83f4f5ae85a7ac7f9651300e1100684241
andriand06 <64359593+andriand06@users.noreply.github.com>
2023-04-10 13:04:11 +0700

    added hero api and hero form

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,12 @@ router.post("/signin", adminController.actionLogin);
 router.use(auth);
 router.get("/logout", adminController.actionLogout);
 router.get("/dashboard", adminController.viewDashboard);
+
+router.get("/hero", adminController.viewHero);
+router.post("/hero", adminController.addHero);
+router.put("/hero", adminController.updateHero);
+router.delete("/hero", adminController.deleteHero);
+
 router.get("/about", adminController.viewAbout);
 router.post("/about", adminController.addAbout);
 router.put("/about", adminController.updateAbout);
