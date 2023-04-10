5351830ecb3e1878e8963915412e52c15543b0a6
andriand06 <64359593+andriand06@users.noreply.github.com>
2023-04-10 13:04:31 +0700

    added hero api and hero form

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const apiController = require("../controllers/apiController");
 
+router.get("/hero", apiController.hero);
 router.get("/about", apiController.about);
 router.get("/experience", apiController.experience);
 router.get("/work", apiController.work);
