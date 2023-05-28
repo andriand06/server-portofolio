284cc06ec16ab706244fca2fb22338a0fe823ba2
andriand06 <64359593+andriand06@users.noreply.github.com>
2023-05-28 11:47:10 +0700

    .

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,6 +3,6 @@ import router from "../../routes/api";
 import serverless from "serverless-http";
 export async function handler(event, context) {
   const api = express();
-  api.use("/api/", router);
+  api.use("/api", router);
   return serverless(api)(event, context);
 }
