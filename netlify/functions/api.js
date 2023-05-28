95403560c4b19414278a13475490a1bf4c4ad889
andriand06 <64359593+andriand06@users.noreply.github.com>
2023-05-28 11:29:36 +0700

    .

--- netlify/functions/api.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import router from "../../routes/api";
-export async function handler(event, context) {
-  const api = express();
-  api.use("/api/", router);
-  return serverless(api)(event, context);
-}
